bab03da1611bcc1d697987f8d25c11add0976ad7
Misuneu <masekmi22@sps-prosek.cz>
2026-03-12 18:47:55 +0100

    fix

--- Pixel/src/Pages/LeaderboardPage.tsx
+++ Pixel/src/Pages/LeaderboardPage.tsx
@@ -13,9 +13,8 @@ export function LeaderboardPage() {
     const loadScores = async () => {
     try {
       const res = await fetch("https://masekmi22.sps-prosek.cz/_Web/API_Pixel/get_scores.php");
-      const data = await res.text()
-      console.log(data)
-     // setScores(data)
+      const data = await res.json()
+      setScores((data))
     } catch (err) {
       console.error("Chyba při načítání:", err)
     }
